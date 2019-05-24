3b69d12532d82f4fe990eb339893e598b6a21d60
Gavin <gavinf902@gmail.com>
2019-05-24 08:24:15 -0700

    ?

---
+++ cdemo/selection.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+
+  // if statement to test is a is equal to 0
+  if (a == 0)
+  {
+    printf("a is 0\n");
+  }
+  else
+  {
+    printf("a is not 0\n");
+  }
+
+
+    if (a >= 0)
+  {
+    printf("a is greater than or equal to 0\n");
+  }
+  else
+  {
+    printf("a is less than 0\n");
+  }
+
+
+
+    if (a != 0)
+  {
+    printf("a is not 0\n");
+  }
+  else
+  {
+    printf("a is 0\n");
+  }
+
+    
+}
+
